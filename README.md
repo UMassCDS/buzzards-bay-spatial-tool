b1d7f3c71c3e40e48d267afd0d95afa1a585cd76
Ozel Yilmazel <ozelyilmazel@vl965-172-31-249-101.wireless.umass.edu>
2024-11-19 10:07:37 -0500

    test data, readme updates

--- README.md
+++ README.md
@@ -3,8 +3,23 @@
 A tool used to collect spatial annotations in interviews.
 
 ## Installation
-1. `npm i`
+To get started you just have install node.js
+
+Go to: https://nodejs.org/en and install node.js, choose your configurations if you are prompted to do so.
+
+After installation check if it's installed correctly.
+
+Run `node --version` in a new terminal window, if you are seeing a version number it is installed correctly.
+
+Carry on with the set up of the application.
+
+Navigate to project root and run `npm i`.
+
+That is all!
 
 ## How to run
-1. `npm run dev`
-2. It will tell you it is running on `localhost:5173`
+
+1. Navigate to project root.
+2. `npm run dev`
+3. It will tell you it is running on `localhost:5173`
+4. Navigate to that url on your browser and start using. Instructions about the application is found inside the application by clicking on the dock icon.
--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -8,11 +8,24 @@ function Legend() {
   return (
     <>
       <Drawer opened={opened} onClose={close} title="How to use this app?">
-        <h1>Instructions</h1>
+        <h2>Instructions</h2>
+        <p>
+          To start an interview, you have to provide an interviewee code,
+          defined by the researchers.
+        </p>
+        <p>
+          On the left side you have fields you can fill in. Annotation type
+          classifies your annotation. Annotation notes denote information you
+          want to share about the selected region. To select a region on the
+          map, you can click to place hexagons, and click again to remove a
+          hexagon. You can also multiselect a large region. On the top left of
+          the map, click on the hexagon symbol, this will allow you to place
+          points on a map to draw a polygon. The rectangle symbol draws a
+          rectangle and deletes hexagons in the covered region.
+        </p>
+        <h2>Manual</h2>
         <p>To be filled out...</p>
-        <h1>Manual</h1>
-        <p>To be filled out...</p>
-        <h1>Legend</h1>
+        <h2>Legend</h2>
         <p>White markers represent low sensor value: 0</p>
         <p>Red markers represent high sensor value: 1</p>
       </Drawer>
